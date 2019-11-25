4b8ba93aeb242443037071cdb9d0a68d2057c09d
Anna Flavia <annaflaviac@me.com>
2019-11-25 15:52:05 -0300

    Aula 1 - CRUD de Pokémons

---
+++ aula1/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const pokemons = require('./routes/pokemons')
+const PORT = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/pokemons', pokemons)
+
+app.get('/', (request, response) => {
+  response.send('Olá, mundo!')
+})
+
+
+app.listen(PORT)
+console.info(`Rodando na porta ${PORT}`)
---
+++ aula1/controllers/PokemonsController.js
@@ -0,0 +1,89 @@
+const { connect } = require('../models/Repository')
+const pokemonsModel = require('../models/PokemonsSchema')
+
+connect()
+
+const getAll = (request, response) => {
+  pokemonsModel.find((error, pokemons) => {
+    if (error) {
+      return response.status(500).send(error)
+    }
+
+    return response.status(200).send(pokemons)
+  })
+}
+
+const getById = (request, response) => {
+  const id = request.params.id
+
+  return pokemonsModel.findById(id, (error, pokemon) => {
+    if (error) {
+      return response.status(500).send(error)
+    }
+
+    if (pokemon) {
+      return response.status(200).send(pokemon)
+    }
+
+    return response.status(404).send('Pokémon não encontrado.')
+  })
+}
+
+const add = (request, response) => {
+  const novoPokemon = new pokemonsModel(request.body)
+
+  novoPokemon.save((error) => {
+    if (error) {
+      return response.status(500).send(error)
+    }
+
+    return response.status(201).send(novoPokemon)
+  })
+}
+
+const remove = (request, response) => {
+  const id = request.params.id
+
+  pokemonsModel.findByIdAndDelete(id, (error, pokemon) => {
+    if (error) {
+      return response.status(500).send(error)
+    }
+
+    if (pokemon) {
+      return response.status(200).send(id)
+    }
+
+    return response.status(404).send('Pokémon não encontrado.')
+  })
+}
+
+const update = (request, response) => {
+  const id = request.params.id
+  const pokemonUpdate = request.body
+  const options = { new: true }
+
+  pokemonsModel.findByIdAndUpdate(
+    id,
+    pokemonUpdate,
+    options,
+    (error, pokemon) => {
+      if (error) {
+        return response.status(500).sned(error)
+      }
+
+      if (pokemon) {
+        return response.status(200).send(pokemon)
+      }
+
+      return response.status(404).send('Pokémon não encontrado.')
+    }
+  )
+}
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  remove,
+  update
+}
