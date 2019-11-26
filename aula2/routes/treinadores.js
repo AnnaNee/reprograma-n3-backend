ed793b2f6429cdab9200412db093873607ba4f8c
Anna Flavia <annaflaviac@me.com>
2019-11-26 18:14:32 -0300

    Assuntos da aula

---
+++ aula2/routes/treinadores.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require("../controllers/TreinadoresController")
+
+
+router.get('', controller.getAll)
+router.post ('', controller.add)
+router.get('/:id', controller.getById)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.remove)
+
+module.exports = router
