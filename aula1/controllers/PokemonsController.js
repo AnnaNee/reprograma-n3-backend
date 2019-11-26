3ee903d1dfc7b9702c472710937256bbc1174e94
JulianaMac <juliana.s.machado@hotmail.com>
2019-11-25 22:31:26 -0300

    calcular nivel pokemon

--- aula1/controllers/PokemonsController.js
+++ aula1/controllers/PokemonsController.js
@@ -80,10 +80,38 @@ const update = (request, response) => {
   )
 }
 
+const updateTreino = (request, response) => {
+  const id = request.params.id
+  const inicial = request.body.inicial
+  const final = request.body.final
+  const options = { new: true }
+
+  const level = (Math.abs(new Date(inicial) - new Date(final)) / 3600000) / 4
+
+  pokemonsModel.findByIdAndUpdate(
+    id,
+    {nivel: level},
+    options,
+    (error, pokemon) => {
+      if (error) {
+        return response.status(500).send(error)
+      }
+
+      if (pokemon) {
+        return response.status(200).send(pokemon)
+      
+      }
+      return response.status(404).send('Pokémon não encontrado.')
+    }
+  )
+}
+
+
 module.exports = {
   getAll,
   getById,
   add,
   remove,
-  update
+  update,
+  updateTreino
 }
