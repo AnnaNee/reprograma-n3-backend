ce42e361732f372892cc6578f582a0ed4d93153e
Anna Flavia <annaflaviac@me.com>
2019-12-05 16:05:02 -0300

    Adiciona função middleware para autenticar

--- aula3/routes/treinadores.js
+++ aula3/routes/treinadores.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const router = express.Router();
-
 const controller = require("../controllers/TreinadoresController")
+const jwt = require('jsonwebtoken')
+const SEGREDO = 'MIICXAIBAAKBgQCOl54HaBM/WiL/jPPdFGjm9f8VprUst1J+vs7G/YRGRHYLGqt+M/ljAhcROPy3FdaVi2smqqyZhf4d+EZ9lKM6LVed91sxvcyMFEp6x8R2KS9wIzUtJ6r1MAIKd8HURmbaN4V2TV/FLeOUANRCZ+QhYEy+eNbuVIJANYtXBUSn8QIDAQABAoGBAIuVS/MAJGdNuxjiSA5Q3mfIw03UhWIiirTb39rXbNbESbGRB/NguW38K8yGNoya6hY2BkwxowgeLKX11js0d5sSHgEgL+pDQtXshHu7vlYU0ksHwfmD/R8+ZHJH6F6L0vuzs4NoVK/8iQHFLboUjF2sORyuLHbBmFZQWhInet8pAkEA0OlL2uHCYhkNuokJ9H+OnJEqKS2BtYSkH3Hrh2opZg2HtvUtXEIxzmj/95CzxMXQtNJhQMK3ekvnF3Upcj2avwJBAK67i8OEKM2jerbFKrBqr6/kUkZeyHLA8I4L2C3/3nKPGUj/GAc2xxuK1XxnpC0e3Wqz5OMwzkWU4Ynblsdq2U8CQHu9U6LICbzVHh6YwP7C9xOhoBlXzPZZJGVDssA4j2DVLsednUqCIsIhy0s1uGUazi3sVpJnQwn7H1vzl6ME/j0CQAT7qj+4LCW5LM27j70aPcppW4NQPq0vHW0fn1moe2KO/CydwcSq5kC909rJZeA3ih755GQqRyeq2EfDMGidfncCQD770Za6sJP1/i1vcdoWuWYnhpiU8TNKjFb2vJEN598amcyJV9PlAAdEkszh6EDA76t6/yT6NoUn/y9x4YskzQo='
 
+const autenticar = (request, response, next) => {
+  const authHeader = request.get('authorization')
+  let autenticado = false
 
-router.get('', controller.getAll)
+  if (!authHeader) {
+    return response.status(401).send('Você precisa fazer login!')
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, SEGREDO, (error, decoded) => {
+    if (error) {
+      autenticado = false
+    } else {
+      autenticado = true
+    }
+  })
+
+  if (!autenticado) {
+    return response.status(403).send('Acesso negado.')
+  }
+
+  next()
+}
+
+router.get('', autenticar, controller.getAll)
 router.post('', controller.add)
-router.get('/:id', controller.getById)
-router.patch('/:id', controller.update)
-router.delete('/:id', controller.remove)
-router.post('/:treinadorId/pokemons', controller.addPokemon)
-router.patch('/:treinadorId/pokemons/:pokemonId/treinar', controller.treinarPokemon)
-router.get('/:id/pokemons', controller.getPokemons)
-router.patch('/:treinadorId/pokemons/:pokemonId', controller.updatePokemon)
-router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
+router.get('/:id', autenticar, controller.getById)
+router.patch('/:id', autenticar, controller.update)
+router.delete('/:id', autenticar, controller.remove)
+router.post('/:treinadorId/pokemons', autenticar, controller.addPokemon)
+router.patch('/:treinadorId/pokemons/:pokemonId/treinar', autenticar, controller.treinarPokemon)
+router.get('/:id/pokemons', autenticar, controller.getPokemons)
+router.patch('/:treinadorId/pokemons/:pokemonId', autenticar, controller.updatePokemon)
+router.get('/:treinadorId/pokemons/:pokemonId', autenticar, controller.getPokemonById)
 router.post('/login', controller.login)
 
 module.exports = router
