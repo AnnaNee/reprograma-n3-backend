4d9cbee9f573dc87286946d7c6668e5027b0c0f1
JulianaMac <juliana.s.machado@hotmail.com>
2019-12-06 21:24:15 -0300

    adicionando arquivos .env na raiz do projeto

--- aula3/app.js
+++ aula3/app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const treinadores = require('./routes/treinadores')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json())
